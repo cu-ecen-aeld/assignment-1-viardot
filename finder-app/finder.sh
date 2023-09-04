785f61078dbf887f8e87b8d52db13a238fabaffc
viardot <e.siebrasse@upcmail.nl>
2023-09-04 08:57:29 +0200

    initial commit

---
+++ finder-app/finder.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+filesdir=$1
+searchstr=$2
+
+
+if [ $# -eq 2 ]; then
+
+  if [ ! -d "$filesdir" ]; then
+	echo "$filesdir is not a directory"
+	exit 1
+  fi
+else
+	echo "Usage: $0 path search"
+	exit 1
+fi
+
+
+
+X=$(ls "$filesdir"|wc -l)
+Y=$(grep -R $searchstr $filesdir/* | wc -l)
+
+
+echo "The number of files are $X and the number of matching lines are $Y"
